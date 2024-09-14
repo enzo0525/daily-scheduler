87bdc594e35e86456fd0a5e10b066451b94a8a33
unknown <loqqty0525@gmail.com>
2024-09-14 14:29:26 -0400

    Add database functionality + fix some bugs

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request
+from database import add_task_database
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/submit", methods=['POST'])
+@app.route('/routines', methods=['GET'])
+def routines_page():
+    return render_template('routines.html')
+
+@app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        print(request.form['taskName'], request.form['taskDate'], request.form['taskNotes'])
+        data = {
+            'taskName': request.form['taskName'],
+            'taskDayOfWeek': request.form['taskDayOfWeek'],
+            'taskNotes': request.form['taskNotes']
+        }
+
+        add_task_database(data)
         return render_template('submit.html')
-    
--- database.py
+++ database.py
@@ -5,15 +5,12 @@ import os
 load_dotenv()
 
 client = pymongo.MongoClient(os.getenv('DATABASE_URL'))
-db = client['daily_scheduler'] #Creates a new database
-routines_collection = db['workouts']  #Creates a new database table inside the actual database
+db = client['daily_scheduler'] #Creates a new database if doesn't exist already
+tasks_collection = db['routine']  #Creates a new database table inside the actual database if doesn't exist already
 
-# routine = {
-#     "user_id": 1,
-#     "description": "Chest day",
-#     "exercises": "x-y-z",
-#     "date": "today"
-# }
+def add_task_database(data):
+    tasks_collection.insert_one(data)
+    print("Routine inserted.")
 
-# routines_collection.insert_one(routine)
-print("Routine inserted.")
+def remove_task_database(dataId): # To do
+    pass
